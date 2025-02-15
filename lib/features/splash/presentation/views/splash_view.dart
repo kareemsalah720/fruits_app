d5f3cd3ec12a20b644a4c01b01a11d8abfe747e2
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2025-02-15 16:48:07 +0200

    create onGenerateRoutes

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+  static const routeName = 'SplashView';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Splash View'),
+      ),
+    );
+  }
+}
