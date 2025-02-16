872d2b86e49bab4c5f21502f930aa57f27f7be99
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2025-02-16 11:31:56 +0200

    Splash View Logic Done

---
+++ lib/features/on_boarding/presentation/view/on_boarding_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingView extends StatelessWidget {
+  const OnBoardingView({super.key});
+  static const String routeName = "onBoardingView";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("On Boarding View")));
+  }
+}
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/features/on_boarding/presentation/view/on_boarding_view.dart';
+import 'package:fruits_app/features/splash/presentation/widgets/splash_view_body.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
   static const routeName = 'SplashView';
+  
 
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Splash View'),
-      ),
+      body: SplashViewBody(),
     );
+  }
+
+  void excuteNavigation() {
+    Future.delayed(const Duration(seconds: 6), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
