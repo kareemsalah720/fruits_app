0b245be4935db90b95144e4fca536d210cf5d8ce
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2025-03-07 23:51:24 +0200

    Login Feature Doing  Custom app bar    Done

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/features/auth/presentation/widget/login_view_body.dart';
+import '../../../../core/functions/custom_app_bar.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
-static const String routeName = "LoginView";
+  static const String routeName = "LoginView";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login View')),
+      appBar: buildAppBar(
+        context,
+        title: 'تسجيل الدخول',
+      ),
+      body: SafeArea(child: LoginViewBody()),
     );
   }
 }
---
+++ lib/features/auth/presentation/widget/login_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
