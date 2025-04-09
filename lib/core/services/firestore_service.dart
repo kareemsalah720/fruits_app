e93dd98332602ee37df42c35eef22afc80eaeae5
Kareem Salah <kareemsalah720@gmail.com>
2025-04-09 21:06:16 +0200

    Create Abstract Database Service&Implement add data method

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,1 +1,11 @@
-class FirestoreService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fruits_app/core/services/database_service.dart';
+
+class FirestoreService implements DatabaseService {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  @override
+  Future<void> addData(
+      {required String path, required Map<String, dynamic> data}) async {
+    await firebaseFirestore.collection(path).add(data);
+  }
+}
