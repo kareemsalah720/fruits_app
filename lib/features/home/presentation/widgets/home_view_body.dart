4bca5f91c3e0e7e9bb370df4bd0d65cb47bb3adc
Kareem Salah <kareemsalah720@gmail.com>
2025-04-21 01:54:16 +0200

    Create featured item

--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fruits_app/constants.dart';
 import 'package:fruits_app/core/widgets/custom_search_text_field.dart';
 import 'package:fruits_app/features/home/presentation/widgets/custom_home_app_bar.dart';
+import 'package:fruits_app/features/home/presentation/widgets/featured_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -16,8 +17,12 @@ class HomeViewBody extends StatelessWidget {
               child: Column(
             children: [
               CustomHomeAppBar(),
-              SizedBox(height: kTopPaddding),
+              SizedBox(height: 16),
               CustomSearchTextField(),
+                SizedBox(
+                  height: 12,
+                ),
+                FeaturedItem(),
             ],
           )),
         ],
