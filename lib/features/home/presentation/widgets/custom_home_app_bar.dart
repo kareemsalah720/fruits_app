54e4e77013038828e1704056cba5e6aa955439b3
Kareem Salah <kareemsalah720@gmail.com>
2025-04-21 00:26:53 +0200

    Create custom home app bar

---
+++ lib/features/home/presentation/widgets/custom_home_app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fruits_app/core/utils/app_images.dart';
+import 'package:fruits_app/core/utils/app_text_styles.dart';
+
+class CustomHomeAppBar extends StatelessWidget {
+  const CustomHomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      trailing:  Container(
+                  width: 34,
+                  height: 34,
+                  decoration: ShapeDecoration(
+                    color: const Color(0xFFEEF8ED),
+                    shape: OvalBorder(),
+                  ),
+                   child: SvgPicture.asset(Assets.imagesNotification),
+                ) ,
+                 title: Text(
+        'صباح الخير !..',
+        textAlign: TextAlign.right,
+        style: TextStyles.regular16.copyWith(
+          color: const Color(0xFF949D9E),
+        ),
+      ),
+      subtitle: const Text(
+        'أحمد مصطفي',
+        textAlign: TextAlign.right,
+        style: TextStyles.bold16,
+      ),
+      leading: Image.asset(Assets.imagesProfileImage),
+    );
+  }
+}
--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/features/home/presentation/widgets/custom_home_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('HomeViewBody'));
+    return CustomHomeAppBar();
   }
 }
