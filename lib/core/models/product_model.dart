22da63284fde1eb0a028430fe748bbad88849a4b
Kareem Salah <kareemsalah720@gmail.com>
2025-05-13 13:25:33 +0300

    create products -> repo &model& entity .

---
+++ lib/core/models/product_model.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+import 'package:fruits_app/core/entites/product_entity.dart';
+import 'review_model.dart';
+
+class ProductModel {
+  final String name;
+  final String code;
+  final String description;
+  final num price;
+  final File image;
+  final bool isFeatured;
+  String? imageUrl;
+  final int expirationsMonths;
+  final bool isOrganic;
+  final int numberOfCalories;
+  final num avgRating = 0;
+  final num ratingCount = 0;
+  final int unitAmount;
+  final List<ReviewModel> reviews;
+  final num sellingCount;
+  ProductModel(
+      {required this.name,
+      required this.code,
+      required this.description,
+      required this.expirationsMonths,
+      required this.numberOfCalories,
+      required this.unitAmount,
+      required this.reviews,
+      required this.price,
+      required this.isOrganic,
+      required this.image,
+      required this.isFeatured,
+      required this.sellingCount,
+      this.imageUrl});
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      name: json['name'],
+      code: json['code'],
+      description: json['description'],
+      expirationsMonths: json['expirationsMonths'],
+      numberOfCalories: json['numberOfCalories'],
+      unitAmount: json['unitAmount'],
+      sellingCount: json['sellingCount'],
+      reviews: json['reviews'] != null
+          ? List<ReviewModel>.from(
+              json['reviews'].map((e) => ReviewModel.fromJson(e)))
+          : [],
+      price: json['price'],
+      isOrganic: json['isOrganic'],
+      isFeatured: json['isFeatured'],
+      image: File(json['image']),
+      imageUrl: json['imageUrl'],
+    );
+  }
+
+  toJson() {
+    return {
+      'name': name,
+      'code': code,
+      'description': description,
+      'price': price,
+      'isFeatured': isFeatured,
+      'imageUrl': imageUrl,
+      'expirationsMonths': expirationsMonths,
+      'numberOfCalories': numberOfCalories,
+      'unitAmount': unitAmount,
+      'isOrganic': isOrganic,
+      'reviews': reviews.map((e) => e.toJson()).toList()
+    };
+  }
+}
